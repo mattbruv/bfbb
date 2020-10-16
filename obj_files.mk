95b61b87f9c6414be4c3fdeeaf3bc0c5d8d9acb2
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-16 09:50:11 -0400

    zGameExtras.s

--- obj_files.mk
+++ obj_files.mk
@@ -163,6 +163,7 @@ TEXT_O_FILES :=                                     \
     $(OBJ_DIR)/asm/Game/zGame.o                     \
     $(OBJ_DIR)/src/Game/zGame.o                     \
     $(OBJ_DIR)/asm/Game/zGameExtras.o               \
+    $(OBJ_DIR)/src/Game/zGameExtras.o               \
     $(OBJ_DIR)/asm/Game/zGameState.o                \
     $(OBJ_DIR)/src/Game/zGameState.o                \
     $(OBJ_DIR)/asm/Game/zGust.o                     \
