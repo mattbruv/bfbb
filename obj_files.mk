6c50f5084a1d14ec940740ad3a9d89bb20e98c29
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-16 08:23:23 -0400

    xent

--- obj_files.mk
+++ obj_files.mk
@@ -29,6 +29,7 @@ TEXT_O_FILES :=                                     \
     $(OBJ_DIR)/asm/Core/x/xDebug.o                  \
     $(OBJ_DIR)/src/Core/x/xDebug.o                  \
     $(OBJ_DIR)/asm/Core/x/xEnt.o                    \
+    $(OBJ_DIR)/src/Core/x/xEnt.o                    \
     $(OBJ_DIR)/asm/Core/x/xEntDrive.o               \
     $(OBJ_DIR)/src/Core/x/xEntDrive.o               \
     $(OBJ_DIR)/asm/Core/x/xEntMotion.o              \
