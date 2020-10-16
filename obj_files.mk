793c2845a96bee5dfa7444643b4ce7915abc79bb
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-16 10:21:54 -0400

    robot

--- obj_files.mk
+++ obj_files.mk
@@ -277,6 +277,7 @@ TEXT_O_FILES :=                                     \
     $(OBJ_DIR)/asm/Game/zNPCTypeCommon.o            \
     $(OBJ_DIR)/src/Game/zNPCTypeCommon.o            \
     $(OBJ_DIR)/asm/Game/zNPCTypeRobot.o             \
+    $(OBJ_DIR)/src/Game/zNPCTypeRobot.o             \
     $(OBJ_DIR)/asm/Game/zNPCTypeVillager.o          \
     $(OBJ_DIR)/asm/Game/zNPCTypeAmbient.o           \
     $(OBJ_DIR)/src/Game/zNPCTypeAmbient.o           \
