62d6cbd909055fa7317bb90d0991967e736bd672
Matt <5638426+mattbruv@users.noreply.github.com>
2020-12-01 09:03:44 -0500

    xvec3init

--- src/Core/x/xClimate.cpp
+++ src/Core/x/xClimate.cpp
@@ -1,6 +1,8 @@
-#include "xClimate.h"
-
 #include <types.h>
+#include "xClimate.h"
+#include "xMath3.h"
+
+extern void memcpy(void* a, const void*b, int32 size);
 
 // func_8000E96C
 #pragma GLOBAL_ASM("asm/Core/x/xClimate.s", "xClimateVecFromAngle__FfP5xVec3")
@@ -30,10 +32,22 @@
 #pragma GLOBAL_ASM("asm/Core/x/xClimate.s", "xClimateUpdate__FP11_tagClimatef")
 
 // func_8000EFFC
-#pragma GLOBAL_ASM("asm/Core/x/xClimate.s", "xVec3Init__FP5xVec3fff")
+void xVec3Init(xVec3* vec, float32 x, float32 y, float32 z)
+{
+    vec->x = x;
+    vec->y = y;
+    vec->z = z;
+}
 
 // func_8000F00C
 #pragma GLOBAL_ASM("asm/Core/x/xClimate.s", "xMat3x3Identity__FP7xMat3x3")
 
 // func_8000F034
+#if 1
 #pragma GLOBAL_ASM("asm/Core/x/xClimate.s", "xMat3x3Copy__FP7xMat3x3PC7xMat3x3")
+#else
+void xMat3x3Copy(xMat3x3* m1, const xMat3x3* m2)
+{
+    memcpy(m1, m2, sizeof(xMat3x3));
+}
+#endif
